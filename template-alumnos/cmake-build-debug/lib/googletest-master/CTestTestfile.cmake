6e607c3e55e214b067394276fbcd36947d5220af
Nicolas Ian Rozenberg <nrozenberg@kovix-group.com>
2021-11-07 16:10:15 -0300

    commited build-debug

--- template-alumnos/cmake-build-debug/lib/googletest-master/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/ignac/Escritorio/TPI_AED11_C11_Grupo6/template-alumnos/lib/googletest-master
-# Build directory: C:/Users/ignac/Escritorio/TPI_AED11_C11_Grupo6/template-alumnos/cmake-build-debug/lib/googletest-master
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- template-alumnos/cmake-build-debug/lib/googletest-master/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/ignac/Escritorio/TPI_AED11_C11_Grupo6/template-alumnos/lib/googletest-master/googletest
-# Build directory: C:/Users/ignac/Escritorio/TPI_AED11_C11_Grupo6/template-alumnos/cmake-build-debug/lib/googletest-master/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
